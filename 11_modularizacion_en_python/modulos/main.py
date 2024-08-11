d560924d80dea0e521b3b0bd02464f9a4512134b
LicetLemus <161054695+LicetLemus@users.noreply.github.com>
2024-08-10 20:12:47 -0500

    chore: Actualizar módulo principal _name_

--- 11_modularizacion_en_python/modulos/main.py
+++ 11_modularizacion_en_python/modulos/main.py
@@ -5,10 +5,15 @@ from mi_paquete import modulo1
 # from datetime import datetime as dt
 # from datetime import *
 
-hoy = dt.date.today()
-ahora = dt.datetime.now()
+if __name__ == '__main__': # si el modulo se ejecuta como principal
+    print('Este es el modulo principal')
+    hoy = dt.date.today()
+    ahora = dt.datetime.now()
 
-print(hoy)
-print(ahora)
+    print(hoy)
+    print(ahora)
 
-modulo1.saludar()
+    modulo1.saludar()
+
+    print(__name__) # __name__ es una variable especial que nos dice el nombre del modulo en el que estamos trabajando
+
--- 11_modularizacion_en_python/modulos/mi_paquete/modulo1.py
+++ 11_modularizacion_en_python/modulos/mi_paquete/modulo1.py
@@ -1,8 +1,9 @@
 # paquetes: estos son directorios que contienen módulos, modulo1.py es un módulo de mi_paquete
-from mi_paquete import variable_global
+# from mi_paquete import variable_global
 
 def saludar():
     print('Hola, soy el módulo 1')
     
     
-print(variable_global)
+# print(variable_global)
+print(__name__)
