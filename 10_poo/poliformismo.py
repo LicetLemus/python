9c71259fa1ccec24c3586124eeac87b9fb05b10c
LicetLemus <161054695+LicetLemus@users.noreply.github.com>
2024-08-10 19:26:14 -0500

    chore: Implementar método __str__ en la clase  para representar el objeto como una cadena ademas de ejemplo de uso para poliformismo

--- 10_poo/poliformismo.py
+++ 10_poo/poliformismo.py
@@ -1,5 +1,4 @@
-# herencia en python: es la capacidad de una clase de heredar atributos y metodos de otra clase padre.
-# las subclases heredan todos los metodos y atributos de la clase padre.
+#poliformismo es la capacidad de un objeto de tomar diferentes formas o comportamientos segun el contexto
 
 class Animal:
     def __init__(self, especie, edad) -> None: # constructor de la clase Animal con atributos
@@ -16,8 +15,25 @@ class Mascota(Animal):
         Animal.__init__(self, especie, edad)
         self.nombre = nombre
         
+    def hablar(self):
+        pass
+    
     def __str__(self):
         return f"Especie: {self.especie}, Edad: {self.edad}, Nombre: {self.nombre}"
         
 mascota1 = Mascota('Felino', 5, 'Mishi')
 print(mascota1.__str__())
+
+class Perro(Mascota):
+    def hablar(self):
+        return "Woof!"
+
+class Gato(Mascota):
+    def hablar(self):
+        return "Miau!"
+    
+p = Perro('Canino', 10, 'Firulais')
+g = Gato('Felino', 5, 'Miaugu')
+
+print(p.hablar())
+print(g.hablar())
