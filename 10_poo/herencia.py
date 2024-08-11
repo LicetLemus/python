6535fb0f4d323f23bfe26cd3a8c67b9350008963
LicetLemus <161054695+LicetLemus@users.noreply.github.com>
2024-08-10 19:20:28 -0500

    chore: Agregar una subclase y ejemplo de herencia en Python

---
+++ 10_poo/herencia.py
@@ -0,0 +1,23 @@
+# herencia en python: es la capacidad de una clase de heredar atributos y metodos de otra clase padre.
+# las subclases heredan todos los metodos y atributos de la clase padre.
+
+class Animal:
+    def __init__(self, especie, edad) -> None: # constructor de la clase Animal con atributos
+        self.especie = especie
+        self.edad = edad
+        
+    def str (self): # metodo especial que retorna una cadena
+        return f"Especie: {self.especie}, Edad: {self.edad}"
+        
+# subclase de la clase Animal
+
+class Mascota(Animal):
+    def __init__(self, especie, edad, nombre) -> None:
+        Animal.__init__(self, especie, edad)
+        self.nombre = nombre
+        
+    def info(self):
+        print(f"Especie: {self.especie}, Edad: {self.edad}, Nombre: {self.nombre}")
+        
+mascota1 = Mascota('Felino', 5, 'Mishi')
+print(mascota1.info())
