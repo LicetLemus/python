6a6f003534065c1c8a83799932543556e6780235
LicetLemus <161054695+LicetLemus@users.noreply.github.com>
2024-08-10 19:48:24 -0500

    chore: Agregar módulo de modularización en Python y ejemplos de uso

---
+++ 11_modularizacion_en_python/modularizacion.py
@@ -0,0 +1,24 @@
+# modularizacion en python consiste en dividir un programa en modulos o archivos para facilitar su mantenimiento y reutilizacion
+# para modularizar un programa en python se debe dividir el codigo en archivos .py y luego importar las funciones o clases que se necesiten
+
+
+# modulos de python mas usados:
+"""
+Os -> permite interactuar con el sistema operativo
+Sys -> permite interactuar con el interprete de python
+math -> funciones matematicas
+random -> funciones para generar numeros aleatorios
+datetime -> funciones para trabajar con fechas y horas
+"""
+
+# Modulos de terceros mas usados:
+
+"""
+requests -> permite hacer peticiones http
+pandas -> permite trabajar con dataframes
+matplotlib -> permite hacer graficos
+numpy -> permite trabajar con arreglos
+scikit-learn -> biblioteca de aprendizaje automatico
+flask -> framework para crear aplicaciones web
+django -> framework para crear aplicaciones web
+"""
