7af466383bd9d78d030682d94159408c76e3a112
LicetLemus <161054695+LicetLemus@users.noreply.github.com>
2024-08-10 19:53:37 -0500

    chore: Agregar importación de módulo datetime y ejemplos de uso

---
+++ 11_modularizacion_en_python/modulos/main.py
@@ -0,0 +1,10 @@
+import datetime as dt # importamos el modulo datetime de python para trabajar con fechas y horas
+# from datetime import date as d
+# from datetime import datetime as dt
+# from datetime import *
+
+hoy = dt.date.today()
+ahora = dt.datetime.now()
+
+print(hoy)
+print(ahora)
