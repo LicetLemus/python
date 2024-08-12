d3dbaa8610ec70d8b1667595b40500e5e7a34030
LicetLemus <161054695+LicetLemus@users.noreply.github.com>
2024-08-11 22:32:54 -0500

    chore: Agregar explicación de tipos de errores en Python

---
+++ 12_errores/tipos.py
@@ -0,0 +1,10 @@
+# errores de sintaxis: son errores que se producen cuando se escribe algo que no es correcto en python
+# errores de identacion: son errores que se producen cuando no se respeta la identacion en python
+
+"""
+errores de excepcion: son errores que se producen cuando se ejecuta el programa y se produce un error
+ejemplo de error de excepcion: division por cero - ZeroDivisionError o imprimir una variable que no existe - NameError
+
+sintacticamente puede estar bien, pero al ejecutar el programa se produce un error
+
+"""
