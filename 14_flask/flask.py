d550b314bf5cc07b2f5cdd6851e63fa367c890e8
LicetLemus <161054695+LicetLemus@users.noreply.github.com>
2024-08-19 20:09:19 -0500

    chore: routes con flask, creacion de venv para trabajar con el framework flask - primera app web

---
+++ 14_flask/flask.py
@@ -0,0 +1,8 @@
+# flask: is the framework for web development in python.
+# It is a micro web framework written in python. It is classified as a microframework because it does not require particular tools or libraries.
+
+# some of the features of flask are:
+# 1. Built-in development server and debugger
+# 2. Integrated support for unit testing
+# 3. RESTful request dispatching
+# 4. Jinja2 templating
